a9bce659e210cbcebe291c94c649db10c181d217
carlosmab <carlosm.araujob@gmail.com>
2023-06-19 13:55:42 -0500

    basic structure

---
+++ src/components/ControlPanel.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="control-panel">
+
+  </div>
+</template>
+
+
+<style scoped>
+.control-panel {
+  position: fixed;
+  bottom: 50px;
+  right: 50px;
+  width: 200px;
+  height: 200px;
+  background-color: #ccc;
+  border-radius: 4px;
+}
+</style>
---
+++ src/components/MainScreen.vue
@@ -0,0 +1,17 @@
+
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="main-panel">
+  </div>
+</template>
+
+<style scoped>
+.main-panel {
+  margin: 50px;
+  width: 90vw;
+  height: 80vh;
+  background-color: #ff0000;
+}
+</style>
